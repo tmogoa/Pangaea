4f1f6b0f538585fa6639fe7c31250096f75514c0
levizwannah <levizwannah@gmail.com>
2021-04-06 12:18:11 +0300

    started the edit account procedure

---
+++ App/logic/procedures/editProfile.php
@@ -0,0 +1,16 @@
+<?php
+    require("../classes/utility.class.php");
+    require("../classes/writer.class.php");
+
+    /**
+     * Editing a user profile
+     * We will only require the the email and password be set. Any other that is added by the 
+     * user is under their control.
+     * We will look for new password, first name, last name, phone number- includes country code, nationality.
+     */
+
+     
+    
+
+
+?>
