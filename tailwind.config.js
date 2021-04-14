805844fc92db131972110324fa0e23c29292b577
Tony Mogoa <mogoa.tonny@gmail.com>
2021-04-14 09:03:40 +0300

    Recreated navbar with tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ module.exports = {
     darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {},
+        fontFamily: {
+            sans: ["Inter", "ui-sans-serif"],
+        },
     },
     variants: {
         extend: {},
