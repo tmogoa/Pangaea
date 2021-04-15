60f6035b84d3b921d365ba5cb46c3e18c1bdd253
ngangasusan <suengangaw@gmail.com>
2021-04-15 12:46:13 +0300

    Finishing the login function

--- App/drafts.php
+++ App/drafts.php
@@ -5,7 +5,7 @@
 <!DOCTYPE html>
 <html lang="en">
     <head>
-        <meta charset="UTF-8" />
+    <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
@@ -28,6 +28,7 @@
             src="https://kit.fontawesome.com/1239ccb1ec.js"
             crossorigin="anonymous"
         ></script>
+        <script src="assets/js/main.js"></script>
         <title>Pangaea</title>
     </head>
 <body>
