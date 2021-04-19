541fe9f778344a29592ca891bf0bfdaa7d330b04
ngangasusan <suengangaw@gmail.com>
2021-04-19 10:32:50 +0300

    Complete preferences page

---
+++ App/preference.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8" />
+            <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    
+            <link rel="preconnect" href="https://fonts.gstatic.com" />
+            <link
+                href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;600;700&display=swap"
+                rel="stylesheet"
+            />
+    
+            <link
+                rel="icon"
+                href="assets/img/logo.svg"
+                type="image/svg"
+                sizes="16x16"
+            />
+            <link rel="stylesheet" href="./assets/css/style.css" />
+    
+            <script
+                src="https://kit.fontawesome.com/1239ccb1ec.js"
+                crossorigin="anonymous"
+            ></script>
+            <script src="assets/js/main.js"></script>
+            <title>Pangaea</title>
+        </head>
+        <body>
+            <div class="flex justify-center p-6 ">
+                <a href="index.php" class="flex items-center">
+                    <img
+                        src="./assets/img/logo.svg"
+                        alt="logo"
+                        class="w-10 sm:w-12 mr-2"
+                    />
+                    <span class="text-blue-900 text-xl">Pangaea</span>
+                </a>
+            </div>  
+            
+            <p class="text-gray-500 text-xl text-center">What would you like to read?</p>
+
+                <ul class="grid grid-flow-col grid-cols-3 grid-rows-4 gap-2 m-4">
+                    <li class="m-4 p-2 border-2 rounded-md ">Software engineering</li>
+                    <li class="m-4 p-2 border-2 rounded-md ">Software engineering</li>
+                    <li class="m-4 p-2 border-2 rounded-md ">Software engineering</li>
+                    <li class="m-4 p-2 border-2 rounded-md ">Software engineering</li>
+                    <li class="m-4 p-2 border-2 rounded-md ">Software engineering</li>
+                    <li class="m-4 p-2 border-2 rounded-md ">Software engineering</li>
+                    <li class="m-4 p-2 border-2 rounded-md ">Software engineering</li>
+                    <li class="m-4 p-2 border-2 rounded-md ">Software engineering</li>
+                    <li class="m-4 p-2 border-2 rounded-md ">Software engineering</li>
+                    <li class="m-4 p-2 border-2 rounded-md ">Software engineering</li>
+                    <li class="m-4 p-2 border-2 rounded-md ">Software engineering</li>
+                    <li class="m-4 p-2 border-2 rounded-md ">Software engineering</li>
+                    
+                </ul>
+
+
+            </div>
+            
+        </body>
+</html>
