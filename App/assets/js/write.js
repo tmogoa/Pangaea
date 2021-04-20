503a3a4058805c5c4a156616be34da88ce016871
Tony Mogoa <mogoa.tonny@gmail.com>
2021-04-20 10:51:54 +0300

    tony's commit

---
+++ App/assets/js/write.js
@@ -0,0 +1,139 @@
+let editor;
+let timeoutId;
+let parser;
+$(function () {
+    parser = edjsHTML();
+
+    $("#title").change(function () {
+        if ($("#title").val() != "") {
+            $("#title_label").css("display", "block");
+        } else {
+            $("#title_label").css("display", "none");
+        }
+    });
+    $("#subtitle").change(function () {
+        if ($("#subtitle").val() !== "") {
+            $("#subtitle_label").css("display", "block");
+        } else {
+            $("#subtitle_label").css("display", "none");
+        }
+    });
+    $("#navbar").load("./components/navbar.php");
+
+    editor = new EditorJS({
+        /**
+         * Id of Element that should contain the Editor
+         */
+        holder: "editorjs",
+
+        /**
+         * Available Tools list.
+         * Pass Tool's class or Settings object for each Tool you want to use
+         */
+        tools: {
+            header: Header,
+            delimiter: Delimiter,
+            paragraph: {
+                class: Paragraph,
+                inlineToolbar: true,
+                config: {
+                    placeholder: "OK, write something binge-worthy...",
+                },
+            },
+            embed: {
+                class: Embed,
+                inlineToolbar: true,
+            },
+            // image: SimpleImage,
+            image: {
+                class: ImageTool,
+                config: {
+                    endpoints: {
+                        byFile: "http://localhost:8008/uploadFile", // Your backend file uploader endpoint
+                        byUrl: "http://localhost:8008/fetchUrl", // Your endpoint that provides uploading by Url
+                    },
+                },
+            },
+        },
+        embed: Embed,
+        image: SimpleImage,
+        /**
+         * Previously saved data that should be rendered
+         */
+        data: {},
+    });
+});
+
+function saveArticle(elem) {
+    switch (elem.attr("id")) {
+        case "editorjs":
+            editor
+                .save()
+                .then((output) => {
+                    //getting json from the editor
+
+                    //test code
+                    showLoader(true);
+                    setTimeout(() => {
+                        showLoader(false);
+                    }, 3000);
+                    //end of test code
+
+                    $("#parsedText").html(parser.parse(output));
+
+                    //console.log("data:" + output);
+                })
+                .catch((error) => {
+                    console.log("error:" + error);
+                });
+            break;
+        case "title":
+            showLoader(true);
+            setTimeout(() => {
+                showLoader(false);
+            }, 3000);
+            console.log("title");
+            break;
+        case "subtitle":
+            showLoader(true);
+            setTimeout(() => {
+                showLoader(false);
+            }, 3000);
+            console.log("subtitle");
+            break;
+    }
+}
+
+function showLoader(visible) {
+    if (visible) {
+        $("#loaderContainer").removeClass("justify-end");
+        $("#loaderContainer").addClass("justify-between");
+        $("#loader").removeClass("hidden");
+    } else {
+        $("#loaderContainer").removeClass("justify-between");
+        $("#loaderContainer").addClass("justify-end");
+        $("#loader").addClass("hidden");
+    }
+}
+
+function listenForChanges() {
+    if (timeoutId) {
+        clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+        //save article to db after 1s inactivity
+        saveArticle($(this));
+    }, 1000);
+}
+
+function autosave() {
+    $("#editorjs").keypress(listenForChanges);
+    $("#title").keypress(listenForChanges);
+    $("#subtitle").keypress(listenForChanges);
+}
+
+autosave();
+
+// article id
+//console.log($("#user-id").val());
--- App/write.php
+++ App/write.php
@@ -3,6 +3,7 @@
     if(!isset($_SESSION['userId'])){
         header("Location: login.php");
     }
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -103,127 +104,11 @@
                 </div>
             </div>
         </div>
+
+
+        <input type="text" name="user-id" id="user-id" value="<?php echo $_SESSION['userId'] ?>" hidden>
+
         <!-- JS file injections-->
-        <script>
-            let editor;
-            let timeoutId;
-            let parser;
-            $(function () {
-                parser = edjsHTML();
-
-                $("#title").change(function () {
-                    if ($("#title").val() != "") {
-                        $("#title_label").css("display", "block");
-                    } else {
-                        $("#title_label").css("display", "none");
-                    }
-                });
-                $("#subtitle").change(function () {
-                    if ($("#subtitle").val() !== "") {
-                        $("#subtitle_label").css("display", "block");
-                    } else {
-                        $("#subtitle_label").css("display", "none");
-                    }
-                });
-                $("#navbar").load("./components/navbar.php");
-
-                editor = new EditorJS({
-                    /**
-                     * Id of Element that should contain the Editor
-                     */
-                    holder: "editorjs",
-
-                    /**
-                     * Available Tools list.
-                     * Pass Tool's class or Settings object for each Tool you want to use
-                     */
-                    tools: {
-                        header: Header,
-                        delimiter: Delimiter,
-                        paragraph: {
-                            class: Paragraph,
-                            inlineToolbar: true,
-                            config: {
-                                placeholder:
-                                    "OK, write something binge-worthy...",
-                            },
-                        },
-                        embed:{
-                            class: Embed,
-                            inlineToolbar: true
-                        },
-                        // image: SimpleImage,
-                        image: {
-                            class: ImageTool,
-                            config: {
-                                endpoints: {
-                                    byFile: "http://localhost:8008/uploadFile", // Your backend file uploader endpoint
-                                    byUrl: "http://localhost:8008/fetchUrl", // Your endpoint that provides uploading by Url
-                                },
-                            },
-                        },
-                    },
-                    embed: Embed,
-                    image: SimpleImage,
-                    /**
-                     * Previously saved data that should be rendered
-                     */
-                    data: {},
-                });
-            });
-
-            function saveArticle() {
-                editor
-                    .save()
-                    .then((output) => {
-                        //getting json from the editor
-
-                        //test code
-                        showLoader(true);
-                        setTimeout(() => {
-                            showLoader(false);  
-                        }, 3000);
-                        //end of test code
-
-                        $("#parsedText").html(parser.parse(output));
-
-                        //console.log("data:" + output);
-
-                    })
-                    .catch((error) => {
-                        console.log("error:" + error);
-                    });
-            }
-
-            function showLoader(visible) {
-
-                if(visible){
-                    $("#loaderContainer").removeClass("justify-end");
-                    $("#loaderContainer").addClass("justify-between");
-                    $("#loader").removeClass("hidden");
-                }else{
-                    $("#loaderContainer").removeClass("justify-between");
-                    $("#loaderContainer").addClass("justify-end");
-                    $("#loader").addClass("hidden");
-                }
-                
-            }
-
-            function autosave(){
-                $("#editorjs").keypress(function() {
-                    if(timeoutId){
-                        clearTimeout(timeoutId);
-                    }
-
-                    timeoutId = setTimeout(() => {
-                        //save article to db after 1s inactivity
-                        saveArticle();
-                    }, 1000);
-                });
-            }
-
-            autosave();
-        </script>
         <script src="https://cdn.jsdelivr.net/npm/@editorjs/editorjs@latest"></script>
         <script src="https://cdn.jsdelivr.net/npm/@editorjs/header@latest"></script>
         <script src="https://cdn.jsdelivr.net/npm/@editorjs/paragraph@latest"></script>
@@ -232,5 +117,6 @@
         <script src="https://cdn.jsdelivr.net/npm/@editorjs/delimiter@latest"></script>
         <script src="https://cdn.jsdelivr.net/npm/@editorjs/image@2.3.0"></script>
         <script src="https://cdn.jsdelivr.net/npm/editorjs-html@3.0.3/build/edjsHTML.js"></script>
+        <script src="./assets/js/write.js"></script>
     </body>
 </html>
