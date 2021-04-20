7276c045318ffaaf27e451341ef474503e8a7ef6
levizwannah <levizwannah@gmail.com>
2021-04-20 11:56:13 +0300

    the migration file

--- App/logic/database/migration.php
+++ App/logic/database/migration.php
@@ -37,7 +37,7 @@
                 $stmt1 =  $conn->prepare($sql);
                 $stmt1->execute();
 
-                $sql = "CREATE TABLE ArticleTopics
+                $sql = "CREATE TABLE articleTopics
                 (
 
                 aTopicId INT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
@@ -50,7 +50,7 @@
                 $stmt2->execute();
 
                 //I already made some updates
-                $sql = "CREATE TABLE Article
+                $sql = "CREATE TABLE article
                 (
                 	articleId INT(20) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
                 	writerId INT(20) UNSIGNED NOT NULL,
@@ -70,7 +70,7 @@
                 $stmt3 =  $conn->prepare($sql);
                 $stmt3->execute();
 
-                $sql = "CREATE TABLE ArticleReaction 
+                $sql = "CREATE TABLE articleReaction 
                 (
                 	aReactionId INT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
                 	articleId INT (20) UNSIGNED,
@@ -83,7 +83,7 @@
                 $stmt4 =  $conn->prepare($sql);
                 $stmt4->execute();
 
-                $sql = "CREATE TABLE Reading
+                $sql = "CREATE TABLE reading
                 (
                 	readingId INT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
                 	readerId INT(20) UNSIGNED,
@@ -97,7 +97,7 @@
                 $stmt5 =  $conn->prepare($sql);
                 $stmt5->execute();
 
-                $sql = "CREATE TABLE ArticleTags 
+                $sql = "CREATE TABLE articleTags 
                 (
                 tagRefId INT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
                 articleId INT(20) UNSIGNED,
@@ -110,7 +110,7 @@
                 $stmt6 =  $conn->prepare($sql);
                 $stmt6->execute();
 
-		$sql = "CREATE TABLE ArticleKeywords
+		$sql = "CREATE TABLE articleKeywords
 		(
 		        keywordId INT(20) UNSIGNED PRIMARY KEY AUTO_INCREMENT,
 		        articleId INT(20) UNSIGNED,
