d603449427734fbc93d6059816987ba5a743b3e9
levizwannah <levizwannah@gmail.com>
2021-04-20 08:39:34 +0300

    Working on the upload image functionality

---
+++ App/logic/procedures/uploadImage.php
@@ -0,0 +1,12 @@
+<?php
+    require_once("utility.inc.php");
+
+    /**
+     * Images can be uploaded for feature image purposes or just for the article media
+     * For feature images
+     */
+
+     
+
+
+?>
