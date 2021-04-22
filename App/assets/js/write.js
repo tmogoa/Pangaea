2fc0ddfa93ff6f412fe0fc975281c7fb45664df9
Tony Mogoa <mogoa.tonny@gmail.com>
2021-04-22 10:58:02 +0300

    fixing featured img

--- App/assets/js/write.js
+++ App/assets/js/write.js
@@ -69,13 +69,13 @@ function saveArticle(elem) {
                 .save() //getting json from the editor
                 .then((output) => {
                     showLoader(true);
-                    console.log(parser.getFeaturedImg(output));
+                    const featuredImg = parser.getFeaturedImg(output);
                     $.post(
                         urlToAutoSaver,
                         {
                             id: articleId,
                             body: output,
-                            featuredImg: parser.getFeaturedImg(output),
+                            featuredImg: featuredImg ? featuredImg : "",
                         },
                         function (data) {
                             console.log(data);
