3b7e218e4c50ef64e6455798a3059f0fcc5974b8
Tony Mogoa <mogoa.tonny@gmail.com>
2021-04-24 12:14:30 +0300

    made index.php responsive

--- App/index.php
+++ App/index.php
@@ -62,22 +62,22 @@
 
  ?>
 
-        <div class="flex text-gray-500 flex-row bg-white shadow-md rounded justify-between w-10/12 mx-auto h-96 mt-6">
+        <div class="flex text-gray-500 flex-col md:flex-row bg-white shadow-md rounded justify-between w-10/12 mx-auto md:h-96 mt-6">
 
-            <div class="mb-2 w-11/12 sm:w-6/12 sm:h-full overflow-hidden rounded-tl rounded-bl flex-shrink-0">
+            <div class="w-full h-1/2 mb-2 md:w-6/12 md:h-full overflow-hidden rounded-tl rounded-bl-none rounded-tr md:rounded-tr-none md:rounded-bl flex-shrink-0">
                 <img src="assets/img/programmer.jpeg" class="h-full w-full object-cover" alt="">
             </div>
 
             <div class="flex flex-col mx-4 p-4">
                 <div class="flex flex-row items-center mb-3">
                     <span class="pr-1 text-xs font-semibold">DEVELOPMENT</span>
-                    <span class="mr-1 w-1 h-1 bg-gray-500 rounded-full hidden sm:inline"></span>
+                    <span class="mr-1 w-1 h-1 bg-gray-500 rounded-full"></span>
                     <span class="text-xs">July 2, 2020</span>
                 </div>
                 <div class="w-full flex flex-col mb-3">
-                    <span class="mb-2 text-3xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
-                    <span class="text-lg mb-1">Maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum.</span>
-                    <div class="text-sm whitespace-normal overflow-ellipsis flex-grow-0 px-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
+                    <span class="mb-2 text-lg font-bold md:text-3xl">Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
+                    <span class="text-md md:text-lg mb-1">Maecenas volutpat blandit aliquam etiam erat velit scelerisque in dictum.</span>
+                    <div class="hidden lg:block text-sm whitespace-normal overflow-ellipsis flex-grow-0 px-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</div>
                 </div>
                 <div class="flex-grow flex flex-col justify-end">
                     <div class="flex flex-row items-center">
@@ -91,7 +91,7 @@
 
         </div>
 
-        <div class="grid grid-cols-3 gap-3 text-gray-500 w-10/12 mx-auto mt-6 mb-4">
+        <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-3 text-gray-500 w-10/12 mx-auto mt-6 mb-4">
             <?php 
                 for ($i=0; $i < 10; $i++) { 
             ?>
@@ -99,7 +99,7 @@
                     <div class="bg-white shadow-md p-2 rounded">grid grid-cols-3 gap-4</div>
                     <div class="bg-white shadow-md p-2 rounded">grid grid-cols-3 gap-4</div> -->
                     <div class="flex justify-center items-center">
-                        <div class="flex flex-col p-4 sm:w-96 m-1 border rounded justify-center bg-white shadow-md">
+                        <div class="flex flex-col p-4 sm:w-96 m-1 rounded justify-center bg-white shadow-md">
                             <div class=" mb-2 w-11/12 sm:w-80 sm:h-48 overflow-hidden rounded">
                                 <img src="assets/img/programmer.jpeg" class="h-full w-full object-cover" alt="">
                             </div>
